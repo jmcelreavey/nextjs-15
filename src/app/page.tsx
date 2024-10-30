7af2b7a1f0cabdbfe85e522c0ddddf5ec2e34472
Ali <ali.hussein.pre@gmail.com>
2024-10-30 11:18:07 +0100

    feat: update hero section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,71 @@
-import Link from "next/link";
+import { Button } from "@/components/ui/button";
+import { BiLogoTailwindCss } from "react-icons/bi";
+import {
+  SiTypescript,
+  SiReact,
+  SiShadcnui,
+  SiNextdotjs,
+  SiGithub,
+} from "react-icons/si";
 
+const logos = [
+  {
+    name: "TypeScript 5",
+    Logo: SiTypescript,
+  },
+  {
+    name: "Nextjs 15",
+    Logo: SiNextdotjs,
+  },
+  {
+    name: "React 19 RC",
+    Logo: SiReact,
+  },
+  {
+    name: "Tailwindcss 3",
+    Logo: BiLogoTailwindCss,
+  },
+  {
+    name: "Shadcn",
+    Logo: SiShadcnui,
+  },
+];
 export default function HomePage() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center">
-      <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
-        <h1 className="text-5xl font-extrabold tracking-tight sm:text-[5rem]">
-          Next 15 Starter
-        </h1>
-        <div className="mx-auto">
-          <div className="text-center text-lg">
-            Starter template for Next.js 15 with React 19 RC, TypeScript,
-            Tailwind CSS and Shadcn.
-          </div>
+    <main className="bg-background flex min-h-screen flex-col items-center justify-center">
+      <div className="container flex grow flex-col items-center justify-center gap-12 px-4 py-16">
+        <div className="mx-auto max-w-3xl space-y-4 px-2">
+          <h1 className="text-center text-5xl font-extrabold tracking-tight sm:text-[5rem]">
+            Next 15 Starter
+          </h1>
+          <p className="text-pretty text-center text-lg">
+            Playground starter for trying out latest features
+          </p>
+        </div>
+        <Button asChild variant={"outline"} className="mx-auto">
+          <a href="https://nextjs.org/blog/next-15">
+            <SiGithub /> Fork it
+          </a>
+        </Button>
+
+        <div className="mx-auto mt-12 flex flex-wrap justify-center gap-3">
+          {logos.map(({ name, Logo }) => (
+            <div
+              key={name}
+              className="flex size-36 flex-col items-center justify-center gap-2.5 rounded-full border border-dashed font-semibold"
+            >
+              <Logo size={32} />
+              <span>{name}</span>
+            </div>
+          ))}
         </div>
       </div>
+      <footer className="w-full border-t border-dashed py-4 text-center">
+        Built by{" "}
+        <Button asChild variant={"link"} size="sm">
+          <a href="https://twitter.com/AliHussein_20">Ali H</a>
+        </Button>
+      </footer>
     </main>
   );
 }
